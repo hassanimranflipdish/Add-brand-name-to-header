a7b21603df62d3b7052b2ab1c849c44d100ffc5f
hassanimranflipdish <hassan.imran@flipdish.com>
2025-02-07 16:37:41 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-window.onload = function() {
+if("order" in window.location.href) {
     document.getElementsByClassName("css-gmu3o8")[0].innerHTML += `<div class="css-18gkv6z"><div><p class="MuiTypography-root MuiTypography-body1 css-ko97w9">Dessertly</p></div></div>`;
-    window.alert("Test1");
 }
-
-window.alert("Test2");
